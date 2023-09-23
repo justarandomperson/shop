f7c226c1f30741481d7771fe82e9e270680b6f91
Random <98654848+justarandomperson@users.noreply.github.com>
2023-09-23 21:40:31 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,51 +3,64 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
+const csurf = require('csurf')
+const flash = require('connect-flash')
 
-const app = express();
 
 const errorController = require("./controllers/error")
 const User = require('./models/user')
+
+const MongoDB_URI = 'process.env.DATABASE_URL'
+
+const app = express();
+const store = new MongoDBStore({
+    uri: MongoDB_URI,
+    collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: '14A3CKDcpR', resave: false, saveUninitialized: false, store: store}))
+
+app.use(csurf())
+app.use(flash())
+
+app.use(async (req,res,next) => {
+    try {
+        const user = await User.findById(req.session.user._id)
+        req.user = user
+    }
+    catch {};
+    next()
+})
 
 app.use((req,res,next) => {
-    User.findById('642aaf1369ffe453273909cb')
-        .then(user => {
-            req.user = user
-            next()
-        })
-        .catch(err => console.log(err))
+    res.locals.isAuthenticated =  req.user
+    res.locals.csurfToken = req.csrfToken()
+    next()
 })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes)
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodblink')
-    .then(() => {
-        User.findOne()
-            .then(user => {
-                if (!user) {
-                    const user = new User({
-                        username: 'dummy',
-                        email: 'dummy@gmail.com',
-                        cart: {
-                            items: []
-                        }
-                    })
-                    user.save()
-                }
-            })
-        app.listen(3000)
-    })
-    .catch(err => console.log(err))
+(async () => {
+try {
+    await mongoose.connect(MongoDB_URI)
+    app.listen(3000)
+} catch(err) {
+    console.log(err)
+}
+})()
 
